633a54d017a59e075e22a98fd23003ee4c34239b
Danthuluriyashaswini <2420030016@klh.edu.in>
2025-10-16 10:36:52 +0530

    Add files via upload

---
+++ WEEK-7/data.js
@@ -0,0 +1,35 @@
+export const books = [
+  {
+    title: "Atomic Habits",
+    author: "James Clear",
+    price: 399,
+    availability: "in stock"
+  },
+  {
+    title: "The Psychology of Money",
+    author: "Morgan Housel",
+    price: 299,
+    //availability: "in stock"
+    availability: "out of stock"
+  },
+   {
+    title: "The Art Of Being Alone",
+    author: "Renuka",
+    price: 177,
+    availability: "in stock"
+  },
+  {
+    title: "Ikigai: The Japanese Secret to a Long and Happy Life",
+    author: "Francesc Miralles",
+    price: 349,
+    availability: "out of stock"
+    //availability: "in stock"
+  },
+  {
+    title: "Think Like a Monk",
+    author: "Jay Shetty",
+    price: 320,
+    //availability: "out of stock"
+    availability: "in stock"
+  }
+];
